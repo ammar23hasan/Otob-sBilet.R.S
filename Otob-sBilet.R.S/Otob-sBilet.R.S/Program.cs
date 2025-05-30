2bf7b704e0c5ba147f87a0de6d42216482ef2d32
ammar23hasan <164255404+ammar23hasan@users.noreply.github.com>
2025-05-30 14:51:40 +0300

    Add files via upload

--- Otob-sBilet.R.S/Otob-sBilet.R.S/Program.cs
+++ Otob-sBilet.R.S/Otob-sBilet.R.S/Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Otob_sBilet.R.S;
+using System.Diagnostics.Metrics;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Otobüs Sefer Rezervasyon Sistemi");
+
+        Sefer sefer = new SehirlerarasiSefer("Gaziantep", "Halep");
+
+        while (true)
+        {
+            Console.Clear();
+            Console.WriteLine($"{sefer.Nereden} ---> {sefer.Nereye} Seferi");
+            sefer.Otobus.KoltuklariGoster();
+
+            Console.Write("\nRezervasyon yapmak istediğiniz koltuk numarasını girin (0 = çıkış): ");
+            if (int.TryParse(Console.ReadLine(), out int koltukNo))
+            {
+                if (koltukNo == 0)
+                    break;
+
+                sefer.RezervasyonYap(koltukNo);
+                Console.WriteLine("Devam etmek için bir tuşa basın...");
+                Console.ReadKey();
+            }
+        }
+    }
+}
